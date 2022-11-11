42b81e37c1923f9e18231da941b6614741bb8fc8
ext_lzunigac@falabella.cl <ext_lzunigac@falabella.cl>
2022-11-11 12:21:29 -0300

    validate project does not exist

--- src/main/java/com/jesuzconte/ppmtool/services/ProjectService.java
+++ src/main/java/com/jesuzconte/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.jesuzconte.ppmtool.services;
 
 import com.jesuzconte.ppmtool.domain.Project;
+import com.jesuzconte.ppmtool.exceptions.ProjectIdException;
 import com.jesuzconte.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,22 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists. ");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (null == project) {
+            throw new ProjectIdException("Project ID: " + projectId.toUpperCase() + " does not exist");
+        }
+
+        return project;
     }
 
 }
